d5fe46e827b2e79c7663c4aded6026be593c0ce2
lixianmin <lixianmin@live.cn>
2022-08-14 17:21:17 +0800

    先加一下UIButton类的控件, 然后想想怎么用起来

---
+++ Assets/Code/Unicorn/UI/Editor/UISerializerEditor.cs
@@ -0,0 +1,406 @@
+﻿
+/********************************************************************
+created:    2017-11-27
+author:     lixianmin
+
+*********************************************************************/
+
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Unicorn.UI
+{
+    internal static partial class UISerializerEditor
+    {
+        private static UISerializer.WidgetData _FillWidgetData (Transform root, string key, string name, string type)
+        {
+            var trans = _DeepFindWidget (root, name);
+            if (null == trans)
+            {
+                return null;
+            }
+
+            var item = new UISerializer.WidgetData();
+            item.key  = key;
+            item.name = name;
+            item.type = type;
+
+            if (type == "GameObject")
+            {
+                item.target = trans.gameObject;
+            }
+            else
+            {
+                var target = _GetWidgetTarget (trans, type);
+                item.target = target;
+                if (null != target)
+                {
+//                    if (target is UIText)
+//                    {
+//                        var label = target as UIText;
+//                        if (!string.IsNullOrEmpty(label.GetGUID()))
+//                        {
+//                            item.userdata = label.GetGUID();
+//                        }
+//                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine("[_FillWidgetData()] Can not find a component with name={0}, type={1}", name, type);
+                }
+            }
+
+            return item;
+        }
+
+        private static Component _GetWidgetTarget (Transform trans, string type)
+        {
+            var target = trans.GetComponent(type);
+            if (null == target && type.StartsWith("UI."))
+            {
+                var script = trans.GetComponent(typeof(UISerializer));
+                if (null != script)
+                {
+                    target = script;
+                }
+            }
+
+            return target;
+        }
+
+        private static Transform _DeepFindWidget (Transform father, string name)
+        {
+            if (null != father && null != name)
+            {
+                foreach (var node in _ForEachNode(father))
+                {
+                    if (node.name == name)
+                    {
+                        return node;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static UISerializer.WidgetData _GetWidgetData (IList<UISerializer.WidgetData> dataList, string name, string type)
+        {
+            if (null != dataList)
+            {
+                var count = dataList.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var item = dataList[i];
+                    if (item.name == name && item.type == type)
+                    {
+                        return item;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static void _SavePrefab (Transform root)
+        {
+            EditorUtility.SetDirty(root.gameObject);
+            AssetDatabase.SaveAssets();
+        }
+
+        private static string _SearchLuaScriptPath (string prefabName)
+        {
+            // var uiScriptRoot = os.path.join(UniqueManifest.GetLuaScriptRoot(), "UI");
+            // var searchName = prefabName.Replace("_", string.Empty).ToLower() + ".lua";
+            // Console.WriteLine("prefabName={0}, searchName={1}, uiScriptRoot={2}", prefabName, searchName, uiScriptRoot);
+            //
+            // foreach (var filepath in os.walk(uiScriptRoot, "*.lua"))
+            // {
+            //     var name = Path.GetFileName(filepath).ToLower();
+            //     if (name.EndsWith(searchName))
+            //     {
+            //         return filepath;
+            //     }
+            // }
+
+            return string.Empty;
+        }
+
+        private static string _CutCandidateText (string text)
+        {
+            var match = _GetHeadRegex().Match(text);
+            if (null != match)
+            {
+                var textLength = text.Length;
+                var startIndex = match.Index + match.Length;
+                var index = startIndex;
+
+                var flag = 1;
+                for (; index< textLength; ++index)
+                {
+                    var c = text[index];
+                    if (c == '{')
+                    {
+                        ++flag;
+                    }
+                    else if (c == '}')
+                    {
+                        --flag;
+
+                        if (flag == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                text = text.Substring(startIndex, index - startIndex);
+            }
+
+            return text;
+        }
+
+        private static void _CollectWidgetFromLua (Transform root, List<UISerializer.WidgetData> dataList)
+        {
+            string scriptPath = _SearchLuaScriptPath(root.name);
+            if (!File.Exists(scriptPath))
+            {
+                return;
+            }
+
+            var text = File.ReadAllText(scriptPath);
+            text = _CutCandidateText(text);
+
+            var validGroupCount = 5;
+
+            foreach (Match match in _GetWidgetRegex().Matches(text))
+            {
+                var groups = match.Groups;
+                if (groups.Count != validGroupCount)
+                {
+                    continue;
+                }
+
+                var key  = groups[2].Value;
+                var name = groups[3].Value;
+                var type = groups[4].Value;
+
+                var lastData = _GetWidgetData(dataList, name, type);
+                if (null != lastData)
+                {
+                    Console.Error.WriteLine("[_CollectWidgetFromLua()] Found an old widgetData with the same name={0}, type={1}", name, type);
+                    continue;
+                }
+
+                var currentData = _FillWidgetData (root, key, name, type);
+                if (null == currentData)
+                {
+                    Console.Error.WriteLine("[_CollectWidgetFromLua()] Can not find a widgetData with name = {0} ", name);
+                    return;
+                }
+
+                _AddUniqueWidgetDdata(dataList, currentData);
+            }
+        }
+
+        private static void _AddUniqueWidgetDdata (IList<UISerializer.WidgetData> dataList, UISerializer.WidgetData current)
+        {
+            if (null != dataList)
+            {
+                var count = dataList.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var item = dataList[i];
+                    if (item.target == current.target)
+                    {
+                        return;
+                    }
+                }
+
+                dataList.Add(current);
+            }
+        }
+
+        private static void _FetchLabels (UISerializer script)
+        {
+            var labelList = ListPool<Text>.Spawn();
+
+            foreach (var node in _ForEachNode(script.transform))
+            {
+                var text = node.GetComponent<Text>();
+                if (null != text)
+                {
+                    labelList.Add(text);
+                }
+            }
+
+            script.labels = labelList.ToArray();
+            ListPool<Text>.Recycle(labelList);
+        }
+
+//        [MenuItem("Assets/Test", false, 0)]
+//        private static void _BroadcastControlMenu ()
+//        {
+//            var goSelected = Selection.activeObject as GameObject;
+//            if (null == goSelected)
+//            {
+//                Console.Warning.WriteLine("Please select a prefab.");
+//                return;
+//            }
+//
+//            var sbText = new System.Text.StringBuilder();
+//            foreach (var trans in _ForEachNode(goSelected.transform))
+//            {
+//                sbText.AppendLine(trans.name);
+//            }
+//
+//            Console.WriteLine(sbText);
+//        }
+
+        private static IEnumerable<Transform> _ForEachNode (Transform father)
+        {
+            if (null != father)
+            {
+                yield return father;
+
+                var childCount = father.childCount;
+                for (int i= 0; i< childCount; ++i)
+                {
+                    var child = father.GetChild(i);
+                    var isNotControl = null == child.GetComponent(typeof(UISerializer));
+                    if (isNotControl)
+                    {
+                        foreach (var node in _ForEachNode(child))
+                        {
+                            yield return node;
+                        }
+                    }
+                    else
+                    {
+                        yield return child;
+                    }
+                }
+            }
+        }
+
+        private static void _CollectUITextWithGUID (UISerializer script, List<UISerializer.WidgetData> dataList)
+        {
+            var transform = script.transform;
+
+            foreach (var item in script.labels)
+            {
+                var label = item as UIText;
+                if (null == label || string.IsNullOrEmpty(label.GetGUID()))
+                {
+                    continue;
+                }
+
+                var name = label.name;
+                var type = "UIText";
+                var lastData = _GetWidgetData(dataList, name, type);
+                if (null != lastData)
+                {
+                    continue;
+                }
+
+                var key = name;
+                var currentData = _FillWidgetData(transform, key, name, type);
+                if (null != currentData)
+                {
+                    _AddUniqueWidgetDdata(dataList, currentData);
+                }
+            }
+        }
+
+        private static void _CheckNameDuplication (Transform root)
+        {
+            var traversedTable = new Dictionary<string, Transform>();
+
+            foreach (var child in _ForEachNode(root))
+            {
+                string name = child.name;
+                if (name.Contains("(") || name.Contains(")"))
+                {
+                    string currentPath = child.GetFindPathEx();
+                    Console.Error.WriteLine("\"()\" is not allowed in gameObject names, path={0}/{1}", root.name, currentPath);
+                }
+
+                var transLast = traversedTable.GetEx(name);
+                if (null != transLast)
+                {
+                    string lastPath = transLast.GetFindPathEx();
+                    string currentPath = child.GetFindPathEx();
+                    Console.Error.WriteLine("Duplication name found: lastPath={0}/{1}\n, currentPath={0}/{2}", root.name, lastPath, currentPath);
+                }
+                else
+                {
+                    traversedTable.Add(name, child);
+                }
+            }
+        }
+
+        private static void _CheckEventSystemExistance (Transform transform)
+        {
+            var script = transform.GetComponentInChildren<UnityEngine.EventSystems.EventSystem>(true);
+            if (null != script)
+            {
+                var path = script.transform.GetFindPathEx();
+                Console.Warning.WriteLine("Caution: detect an EventSystem script ({0})", path);
+            }
+        }
+
+        public static void SerializePrefab (UISerializer rootScript)
+        {
+            Console.WriteLine ("Begin serializing **********************");
+
+            var root = rootScript.transform;
+
+            _CheckNameDuplication(root);
+            _CheckEventSystemExistance(root);
+
+            rootScript.widgetDatas = null;
+            var dataList = ListPool<UISerializer.WidgetData>.Spawn();
+            _CollectWidgetFromLua(root, dataList);
+
+            _FetchLabels(rootScript);
+            _CollectUITextWithGUID(rootScript, dataList);
+
+            rootScript.widgetDatas = dataList.ToArray();
+            ListPool<UISerializer.WidgetData>.Recycle(dataList);
+
+            _SavePrefab(root);
+
+            Console.WriteLine ("End serializing **********************");
+        }
+
+        private static Regex _GetHeadRegex ()
+        {
+            if (null == _headRegex)
+            {
+                var pattern = @"^\s*Layout\s*=\s*{";
+                _headRegex = new Regex(pattern, RegexOptions.Multiline);
+            }
+
+            return _headRegex;
+        }
+
+        private static Regex _GetWidgetRegex ()
+        {
+            if (null == _widgetRegex)
+            {
+                var pattern = @"(^\s*(\w+)\s*=\s*\{\s*['""](\w+)['""]\s*,\s*['""](.+?)['""]\s*\}\s*[;,]?)+?";
+                _widgetRegex = new Regex(pattern, RegexOptions.Multiline);
+            }
+
+            return _widgetRegex;
+        }
+
+        private static Regex _headRegex;
+        private static Regex _widgetRegex;
+    }
+}
---
+++ Assets/Code/Unicorn/UI/UISerializer.cs
@@ -0,0 +1,192 @@
+
+/********************************************************************
+created:    2017-06-01
+author:     lixianmin
+
+	1. 将脚本挂在界面的prefab上试用；
+	2. 右键点击脚本组件选择Serialize Prefab操作进行序列化；
+	3. 进行序列化通过prefab名字与界面同名lua类的Layout表中定义的字段匹配进行；
+	4. 序列化会将控件的数据保存在prefab资源中；
+
+	注意：制作界面prefab节点的名字必须唯一；
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unicorn.UI
+{
+    [DisallowMultipleComponent]
+	public partial class UISerializer : MonoBehaviour
+    {
+        [Serializable]
+        public class WidgetData : IComparable<WidgetData>
+        {
+            public int CompareTo (WidgetData rhs)
+            {
+                if (null == rhs)
+                {
+                    return 1;
+                }
+
+                var myName = name ?? string.Empty;
+                var nameCompareResult = myName.CompareTo(rhs.name);
+                if (nameCompareResult != 0)
+                {
+                    return nameCompareResult;
+                }
+
+                var myType = type ?? string.Empty;
+                var typeCompareResult = myType.CompareTo(rhs.type);
+                return typeCompareResult;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("[WidgetData] key={0}, name={1}, type={2}", key, name, type);
+            }
+
+            [HideInInspector]
+            public string key  = string.Empty;
+
+            public string name = string.Empty;
+            public string type = null;
+
+            public UnityEngine.Object target = null;
+
+//            [HideInInspector]
+//            public string userdata = null;
+        }
+
+        private void Awake ()
+        {
+            _ResetLabelsFont();
+            _scripts.Add(this, this);
+        }
+
+        private void Destroy ()
+        {
+            _scripts.Remove(this);
+        }
+
+        public static void SetLocaleFonts (string[] rawFontNames, Font[] localeFonts)
+        {
+            if (null == rawFontNames || rawFontNames.Length == 0)
+            {
+                return;
+            }
+
+            if (null == localeFonts || localeFonts.Length == 0)
+            {
+                return;
+            }
+
+            if (rawFontNames.Length != localeFonts.Length)
+            {
+                return;
+            }
+
+            _rawFontNames = rawFontNames;
+            _localeFonts = localeFonts;
+
+            // refresh all script fonts.
+            var iter = _scripts.GetEnumerator();
+            while (iter.MoveNext())
+            {
+                var entry = (DictionaryEntry) iter.Current;
+                var script = entry.Value as UISerializer;
+                script._ResetLabelsFont();
+            }
+        }
+
+        private void _ResetLabelsFont ()
+        {
+            var count = _rawFontNames.Length;
+            if (null == labels || labels.Length == 0 || count == 0 || _localeFonts.Length != count)
+            {
+                return;
+            }
+
+            var cachedFonts = new Font[count];
+            foreach (var label in labels)
+            {
+                var nextFont = _GetFont(label.font, cachedFonts);
+                if (null != nextFont)
+                {
+                    label.font = nextFont;
+                }
+                else
+                {
+                    Console.Warning.WriteLine("nextFont=null, label={0}, label.font={1}, cachedFonts=[{2}]"
+                        , label.text, label.font, ", ".JoinEx(cachedFonts));
+                    
+                }
+            }
+        }
+
+        private static Font _GetFont (Font currentFont, Font[] cachedFonts)
+        {
+            var count = cachedFonts.Length;
+            for (int i= 0; i< count; ++i)
+            {
+                var cached = cachedFonts[i];
+                if (null != cached)
+                {
+                    if (currentFont == cached)
+                    {
+                        return _localeFonts[i];
+                    }
+                }
+                else  if (currentFont.name == _rawFontNames[i])
+                {
+                    cachedFonts[i] = currentFont;
+                    var retFont = _localeFonts[i];
+                    return retFont;
+                }
+            }
+
+            return null;
+        }
+
+        private void Reset ()
+        {
+            _SerializePrefab();
+        }
+
+        // [ContextMenu("Serialize Prefab")]
+        private void _SerializePrefab ()
+        {
+            if (null == _lpfnSerializePrefab)
+            {
+                var type = _GetUISerializerEditorType();
+                var method = type.GetMethod("SerializePrefab", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+                TypeTools.CreateDelegate(method, out _lpfnSerializePrefab);
+            }
+
+            _lpfnSerializePrefab(this);
+        }
+
+        private static Type _GetUISerializerEditorType ()
+        {
+            var type = TypeTools.SearchType("Unicorn.UI.UISerializerEditor");
+            return type;
+        }
+
+        private static string[] _rawFontNames = EmptyArray<string>.Instance;
+        private static Font[] _localeFonts = null;
+        private static readonly Hashtable _scripts = new Hashtable();
+
+        private static Action<UISerializer> _lpfnSerializePrefab;
+
+        public MonoBehaviour openWindowScript;
+        public MonoBehaviour closeWindowScript;
+		
+        public WidgetData[] widgetDatas = null;
+        public Text[] labels = null;
+    }
+}
