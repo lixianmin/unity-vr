bf1d38ea291a07124e406b6eeaa42aacb4377ff7
lixianmin <lixianmin@live.cn>
2022-08-25 12:39:25 +0800

    adjust number

---
+++ Assets/Code/Client/UI/UIAdjustNumber.cs
@@ -0,0 +1,44 @@
+/********************************************************************
+created:    2022-08-24
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+using Unicorn.UI;
+
+namespace Client.UI
+{
+    public class UIAdjustNumber: UIWindowBase
+    {
+        public override string GetResourcePath()
+        {
+            return "Assets/res/prefabs/uiadjustnumber.prefab";
+        }
+
+        private readonly UIWidget<UIButton> _btnIncr = new( "btn_increment");
+        private readonly UIWidget<UIButton> _btnDecr= new(  "btn_decrement");
+        private readonly UIWidget<UIText> _title = new( "text_number");
+        public override void OnLoaded()
+        {
+            var btnIncr = _btnIncr.GetWidget(this);
+            var btnDecr = _btnDecr.GetWidget(this);
+            var title = _title.GetWidget(this);
+            
+            btnIncr.onClick.AddListener(()=>{
+                title.text = (int.Parse(title.text) + 1).ToString();
+            });
+            
+            btnDecr.onClick.AddListener(()=>{
+                title.text = (int.Parse(title.text) - 1).ToString();
+            });
+            
+            Console.WriteLine("uiadjustnumber is OnLoaded");
+        }
+
+        public override void OnUnloading()
+        {
+            Console.WriteLine("uiadjustnumber is OnUnloading");
+        }
+    }
+}
